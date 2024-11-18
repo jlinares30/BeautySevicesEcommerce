a9926e414f8f79186098d908f5cad39f91be7627
jlinares30 <jorgelinaresrrs306@hotmail.com>
2024-11-18 16:28:57 -0500

    despliegue

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 import reviewRoutes from "./routes/reviewRoutes.js";
-
+import cors from 'cors'
 dotenv.config()
 
 
@@ -23,7 +23,9 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json())
 
-
+// app.use(cors({
+// 	origin: 'http://localhost:3000'
+// }))
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
